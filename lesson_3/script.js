200807902f0f54c4b3aebe91a11f081b7fcffcfa
Dhiraj Sarraf <108226247+dhirajs16@users.noreply.github.com>
2024-10-12 03:32:12 +0000

    Added lesson_3: basic scene with workflow of 3js

---
+++ lesson_3/script.js
@@ -0,0 +1,40 @@
+// console.log(THREE)
+
+const canvas = document.getElementById('webgl')
+
+
+// Scene
+const scene = new THREE.Scene()
+
+// Mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1, 1)
+const material  = new THREE.MeshBasicMaterial({color: 0xff0000})
+const mesh = new THREE.Mesh(geometry, material) //red cube
+scene.add(mesh)
+
+// size and aspect ratio
+const size = {
+    width: 800,
+    height: 600
+}
+const aspect_ratio = size.width/size.height
+
+// camera
+const camera = new THREE.PerspectiveCamera(75, aspect_ratio,) // (fov, aspect_ratio, )
+camera.position.z = 3
+camera.position.y = 1
+scene.add(camera)
+
+// renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(size.width, size.height)
+renderer.render(scene, camera)
+
+
+
+
+
+
+
